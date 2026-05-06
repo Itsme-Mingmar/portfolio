2b9ea286c364bfbc9ed06965d05a8c17ed7980f7
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-05-06 11:19:11 +0545

    Initial commit: Developer portfolio with React + Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+function App() {
+
+  return (
+    <div className="bg-primary text-text-primary min-h-screen">
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
